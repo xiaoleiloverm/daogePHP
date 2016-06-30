48569123466346119c0a0765d1856026fc9d4e47
雷露 <502928809@qq.com>
2016-06-30 17:51:55 +0800

    框架初始化文件

---
+++ library/init.class.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * daogePHP - A PHP Framework For Web
+ *
+ * @author   leilu<xiaoleiloverm@gmail.com>
+ */
+
+//----------------------------------
+// 初始化类文件
+//----------------------------------
+namespace library;
+
+class init
+{
+
+    /**
+     *应用程序初始化
+     */
+    final public static function start()
+    {
+        // 注册AUTOLOAD方法
+        spl_autoload_register('init::autoload');
+        //异常处理
+
+    }
+
+    /**
+     * 类库自动加载
+     * @param string $class 对象类名
+     * @return void
+     */
+    final public static function autoload($class)
+    {
+
+    }
+}
