6f52ad9a33eab413b6706edb94f445fe203633c6
雷露 <502928809@qq.com>
2016-06-30 16:39:02 +0800

    框架初始化文件

---
+++ init.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * daogePHP - A PHP Framework For Web
+ *
+ * @author   leilu<xiaoleiloverm@gmail.com>
+ */
+
+//----------------------------------
+// 初始化文件
+//----------------------------------
+
+//版本
+const DAOGE_VERSION = '1.0.0';
