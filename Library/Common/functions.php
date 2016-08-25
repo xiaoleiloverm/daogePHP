8d8bd9d9589cf273fad0cd09c54b97b6d0aa333d
雷露 <502928809@qq.com>
2016-08-25 16:54:16 +0800

    重新上传新路径对应文件

---
+++ Library/Common/functions.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * daogePHP - A PHP Framework For Web
+ *
+ * @author   leilu<xiaoleiloverm@gmail.com>
+ */
+
+//----------------------------------
+// 核心函数库
+//----------------------------------
+
+/**
+ *配置函数
+ */
+function C()
+{
+
+}
+
+/**
+ *错误处理函数
+ */
+function E()
+{
+    throw new errorException("Error Processing Request", 1);
+
+}
---
+++ Library/Controller/Controller.class.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * +------------------------------------------------------------------
+ * |daogePHP： 框架核心控制器
+ * +------------------------------------------------------------------
+ * |athor：leilu<xiaoleiloverm@gmail.com>
+ * +------------------------------------------------------------------
+ * |Copyright (c) 2016-2018 All rights reserved.
+ * +------------------------------------------------------------------
+ */
+namespace Library\Controller;
+
+class Controller
+{
+
+    /**
+     *加载视图
+     */
+    public function display()
+    {
+        //框架控制器根目录
+        include APP_PATH . 'Viem' . DIRECTORY_SEPARATOR . 'default' . DIRECTORY_SEPARATOR . 'Index' . DIRECTORY_SEPARATOR . 'index.html';
+    }
+
+    /**
+     *
+     */
+    public function test()
+    {
+        echo 'this is test';
+    }
+}
