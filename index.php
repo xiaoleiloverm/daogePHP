a6c82f3f465dab2216fade981c80aacabf2bc22d
雷露 <502928809@qq.com>
2016-09-09 17:56:21 +0800

    框架入口文件

--- index.php
+++ index.php
@@ -13,5 +13,9 @@ if (version_compare(PHP_VERSION, '5.3.0', '<')) {
     die('require PHP > 5.3.0 !');
 }
 
-//核心初始化
+define('APP_PATH', __DIR__ . '/app/'); //应用目录
+define('APP_COMMON_PATH', __DIR__ . '/Common/'); //公共目录
+define('APP_DEBUG', true); //开启调试模式
+
+//加载框架核心
 require './daogePHP/init.php';
