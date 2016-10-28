b747798e3f84505863c9ecddda176afd8aa73538
雷露 <502928809@qq.com>
2016-10-28 12:16:57 +0800

    数据库通用配置

--- Library/Config/tradition.php
+++ Library/Config/tradition.php
@@ -20,6 +20,15 @@ return [
     /* SESSION设置 */
 
     /* 数据库设置 */
+    'DB_TYPE'               => '', // 数据库类型
+    'DB_HOST'               => '', // 服务器地址
+    'DB_NAME'               => '', // 数据库名
+    'DB_USER'               => '', // 用户名
+    'DB_PWD'                => '', // 密码
+    'DB_PORT'               => '', // 端口
+    'DB_PREFIX'             => '', // 数据库表前缀
+    'DB_DEBUG'              => true, // 数据库调试模式 开启后可以记录SQL日志
+    'DB_CHARSET'            => 'utf8', // 数据库编码默认采用utf8
 
     /* 数据缓存设置 */
 
