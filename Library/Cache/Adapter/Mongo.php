47885c60d83a7bf4da604965b2f35879ac123404
雷露 <502928809@qq.com>
2016-11-10 17:58:58 +0800

    mongodb操作类命名

---
+++ Library/Cache/Adapter/Mongo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * +------------------------------------------------------------------
+ * |daogePHP： 框架核心缓存适配器 Mongo MongoDb
+ * +------------------------------------------------------------------
+ * |athor：leilu<xiaoleiloverm@gmail.com>
+ * +------------------------------------------------------------------
+ * |Copyright (c) 2016-2018 All rights reserved.
+ * +------------------------------------------------------------------
+ */
+namespace Library\Cache\Adapter;
+
+use Library\Construct\Cache\AbstractAdapter;
+
+class Mongo extends AbstractAdapter
+{
+
+}
